b690a2d82a4f6b4103856b6218a334382c3aeb8a
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2025-01-08 22:35:05 +0300

    ilk commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Person person1 = new Person("John", "Doe", 20);
+        System.out.println("Firstname: " + person1.getFirstName());
+        System.out.println("LastName: " + person1.getLastName());
+        System.out.println("Age: " + person1.getAge());
+        System.out.println("Is Teen: " + person1.isTeen());
     }
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,4 +1,38 @@
 package org.example;
 
 public class Person {
+    String firstName;
+    String lastName;
+    int age;
+    int height;
+    int weight;
+    String favoriteColor;
+
+    public Person(String firstName,String lastName,int age){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.age=age;
+    }
+    public Person(String firstName,String lastName,int age,int height,int weight,String favoriteColor){
+        this(firstName,lastName,age);
+        this.height=height;
+        this.weight=weight;
+        this.favoriteColor=favoriteColor;
+
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+    public Boolean isTeen(){
+       if(age>=13&&age<=19){
+           return true;
+       }return false;
+    }
+
 }
